4d09927a4aa7eda2d8ac9d729d3a559440a435bd
Sithija Kumarasinghe <138661611+SithijaDeshan@users.noreply.github.com>
2024-11-03 15:59:14 +0530

    Inital commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App'
+
+// mount functio for start up the app
+const mount = (el) => {
+  ReactDOM.render(<App />, el);
+};
+
+//If we are in development and in islation, call mount immediately
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing_dev_root");
+
+  if (devRoot) {
+    mount(devRoot);
+  }
+}
+
+//If we are running through container and we should export the mount function
+export { mount };
