effb0badfc0de591418efb383536434e2ef52946
Sithija Kumarasinghe <138661611+SithijaDeshan@users.noreply.github.com>
2024-11-07 11:38:15 +0530

    fix 4

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -5,22 +5,41 @@ const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
+// const prodConfig = {
+//   mode: "production",
+//   output: {
+//     filename: "[name].[contenthash].js",
+//     publicPath: "container/latest/",
+//   },
+//   plugins: [
+//     new ModuleFederationPlugin({
+//       name: "container",
+//       remotes: {
+//         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
+//         auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+//       },
+//       shared: packageJson.dependencies,
+//     }),
+//   ],
+// };
+
 const prodConfig = {
   mode: "production",
   output: {
     filename: "[name].[contenthash].js",
-    publicPath: "container/latest/",
+    publicPath: `${domain}/container/latest/`,
   },
   plugins: [
     new ModuleFederationPlugin({
       name: "container",
       remotes: {
         marketing: `marketing@${domain}/marketing/latest/remoteEntry.js`,
-        auth: `auth@${domain}/auth/latest/remoteEntry.js`,
+        auth: `auth@${domain}/container/auth/latest/remoteEntry.js`,  // Adjusted if necessary
       },
       shared: packageJson.dependencies,
     }),
   ],
 };
+
 
 module.exports = merge(commonConfig, prodConfig);
